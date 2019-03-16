172b6a750018cb3064db27d60aa60abaaa4a2704
Rishab Nair <rishab9706@gmail.com>
2019-03-16 14:27:58 +0000

    i9152: build light hal

--- device_i9152.mk
+++ device_i9152.mk
@@ -109,6 +109,7 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.light@2.0-impl \
     libbt-vendor \
 
 # HIDL manifest
